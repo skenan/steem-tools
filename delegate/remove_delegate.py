439925cbdf82efa4952eb488957ca8e3a659685f
skenan <skenan1992@gmail.com>
2018-01-21 12:10:35 -0800

    add check vesting tool

--- delegate/remove_delegate.py
+++ delegate/remove_delegate.py
@@ -1,6 +1,8 @@
 from steem import Steem
 from steem.converter import Converter
+from steem.account import Account
 import time
+from datetime import datetime, timedelta
 from config import Key
 
 user = Key['steem_2']['user_name']
@@ -11,7 +13,6 @@ wif = {
 
 def run_delegate():
     s = Steem(keys=wif)
-    # TODO check accounts post
     vests = '{} VESTS'.format(Converter().sp_to_vests(2))
     for u in s.get_vesting_delegations('cnsteem', '', 1000):
         amount, units = u['vesting_shares'].split()
@@ -19,3 +20,19 @@ def run_delegate():
             res = s.delegate_vesting_shares(u['delegatee'], vests, user)
             print("Done:", u['delegatee'])
             time.sleep(1)
+
+
+def check_vesting():
+    s = Steem(keys=wif)
+    date_7_days_ago = str(datetime.now() - timedelta(days=7))
+    vests = '{}'.format(Converter().sp_to_vests(2))
+    for u in s.get_vesting_delegations('cnsteem', '', 1000):
+        acc = Account(u['delegatee'])
+        amount, units = acc['vesting_shares'].split()
+        if acc['created'] < date_7_days_ago and acc['last_post'] < date_7_days_ago :
+            s.delegate_vesting_shares(u['delegatee'], '0 VESTS', user)
+            print(u['delegatee'], acc['created'], acc['last_post'])
+
+        if float(amount) > float(vests):
+            s.delegate_vesting_shares(u['delegatee'], '0 VESTS', user)
+            print(u['delegatee'], float(amount))
---
+++ run_check_vesting.py
@@ -0,0 +1,4 @@
+from delegate import remove_delegate
+
+if __name__ == '__main__':
+    remove_delegate.check_vesting()
